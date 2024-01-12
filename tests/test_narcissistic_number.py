b2cdcff9437317370378e7bc3da537949c25c401
Eric <eric@leanmind.es>
2024-01-12 13:32:35 +0000

    tests

--- tests/test_narcissistic_number.py
+++ tests/test_narcissistic_number.py
@@ -7,10 +7,44 @@ from src.narcissistic_number import is_narcissistic
 
 class TestVendingMachine:
 
+    def invalid_coins_are_not_counted(self):
+        # Given
+        # tengo un penny
+        # When
+        # lo enchufo en la maquina
+        # Then
+        # no me lo añadió al conteo
+
+        pass
+
+    def invalid_coins_are_returned(self):
+        # Given
+        # tengo un penny
+        # When
+        # lo enchufo en la maquina
+        # Then
+        # me devolvió la moneda
+
+        pass
+
+    def nickels_are_counted(self):
+
+        pass
+
+    def nickels_are_not_returned(self):
+
+        pass
+
+    def valid_coins_are_counted(self):
+
+        pass
+
+    def valid_coins_are_not_returned(self):
+
+        pass
+
     def requests_coins_when_there_are_no_coins_inserted(self):
 
         pass
 
-    def rejects_invalid_coins(self):
 
-        pass
