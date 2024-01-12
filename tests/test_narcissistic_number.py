e137fb72680d3bf41f8c732b73ca1dc319454b60
Eric <eric@leanmind.es>
2024-01-12 12:44:02 +0000

    test names

--- tests/test_narcissistic_number.py
+++ tests/test_narcissistic_number.py
@@ -5,13 +5,12 @@ from assertpy import assert_that
 from src.narcissistic_number import is_narcissistic
 
 
-class TestIsNarcissisticNumber(TestCase):
-    def test_is_narcissistic_if_the_sum_of_its_digits_to_the_nth_power_equal_the_original_number(
-        self,
-    ):
-        assert_that(is_narcissistic(150)).is_false()
-        assert_that(is_narcissistic(0)).is_true()
-        assert_that(is_narcissistic(153)).is_true()
+class TestVendingMachine:
 
-    def test_narcissistic_numbers_should_be_positive(self):
-        assert_that(is_narcissistic).raises(ValueError).when_called_with(-1)
+    def requests_coins_when_there_are_no_coins_inserted(self):
+
+        pass
+
+    def rejects_invalid_coins(self):
+
+        pass
