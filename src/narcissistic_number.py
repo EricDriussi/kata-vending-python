d0ea34b207f86d47e704b9c28be744e400847281
Eric <eric@leanmind.es>
2024-01-12 13:48:56 +0000

    first test implementation

--- src/narcissistic_number.py
+++ src/narcissistic_number.py
@@ -1,4 +1,3 @@
-def is_narcissistic(given_number: int) -> bool:
-    digits = [int(n) for n in str(given_number)]
-    calculated_digits = list(map(lambda d: pow(d, len(digits)), digits))
-    return sum(calculated_digits) == given_number
+class VendingMachine():
+    def __init__(self, display):
+        self.display = display
--- tests/test_narcissistic_number.py
+++ tests/test_narcissistic_number.py
@@ -2,16 +2,34 @@ from unittest import TestCase
 
 from assertpy import assert_that
 
-from src.narcissistic_number import is_narcissistic
+from src.narcissistic_number import VendingMachine
+
+class DisplaySpy:
+    insertCoinWasCalled = False
+
+    def insert_coin(self):
+        self.insertCoinWasCalled = True
+
+    def assert_insert_coin_was_called(self):
+        assert_that(self.insertCoinWasCalled).is_true()
 
 
 class TestVendingMachine:
 
+    def requests_coins_when_there_are_no_coins_inserted(self):
+        display = DisplaySpy()
+
+        _ = VendingMachine(display)
+
+        display.assert_insert_coin_was_called()
+        pass
+
     def invalid_coins_are_not_counted(self):
         # Given
-        # tengo un penny
+        # penny = InvalidCoin()
+        # maquinita = VendingMachine()
         # When
-        # lo enchufo en la maquina
+        # maquinita.insert(penny)
         # Then
         # no me lo añadió al conteo
 
@@ -42,9 +60,4 @@ class TestVendingMachine:
     def valid_coins_are_not_returned(self):
 
         pass
-
-    def requests_coins_when_there_are_no_coins_inserted(self):
-
-        pass
-
 
